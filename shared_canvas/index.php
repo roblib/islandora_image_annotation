764c78aa1930e571c5f3ac3119cda2979c94293e
Alan Stanley <astanley@upei.ca>
2012-11-15 08:38:15 -0400

    changed wording in admin panel

--- shared_canvas/index.php
+++ shared_canvas/index.php
@@ -192,7 +192,8 @@
 
           <div id ="color-picker-wrapper" class="element-wrap">
             <label for="anno_color">Color:</label>
-            <input id ="anno_color"type="hidden" name="color4" value="#91843c" class="color-picker" size="7" />
+            <input id ="anno_color" type="hidden" name="color4" value="#91843c" class="color-picker" size="7" />
+            <input id ="anno_color_value" type="hidden" value ="" size="7" />
           </div>
 
 
