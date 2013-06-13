d39400b846f1778a4bd8e334ff5b7b6cf3dfd53c
Paul Pound <ppound@upei.ca>
2013-06-13 15:46:33 -0300

    fixed minor typos

--- theme/islandora-image-annotation-recent-views.tpl.php
+++ theme/islandora-image-annotation-recent-views.tpl.php
@@ -3,7 +3,7 @@
   <?php foreach($variables['items'] as $item): ?> 
     <dt class="islandora-anno-recent-views-link"><?php print $item['link']; ?></dt>
     <dd class="islandora-anno-recent-views-tn">
-      <a href="<?php print $item['url_only']?>">"<?php print $item['TN']; ?></a> 
+      <a href="<?php print $item['url_only']?>"><?php print $item['TN']; ?></a> 
     </dd>  
    <?php endforeach; ?>
     </dl>
